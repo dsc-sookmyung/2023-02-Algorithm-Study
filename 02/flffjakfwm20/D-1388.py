94a4b02d7f055192340051a61252b990624b20f5
abi-hong <flffjakfwm20@gmail.com>
2023-11-09 15:28:03 +0900

    바닥 장식(dfs)

--- 02/flffjakfwm20/D-1388.py
+++ 02/flffjakfwm20/D-1388.py
@@ -6,8 +6,32 @@ rooms = []
 
 for i in range(n):
   rooms.append(list(input().strip()))
-
 result = 0
+
+# dfs
+dx = [-1, 1]
+dy = [-1, 1]
+def dfs(x, y):
+  if (rooms[x][y] == '-'):
+    rooms[x][y] = 1 # 해당 노드 방문처리
+    for i in dy:
+      ny = y + i
+      if (ny > 0 and ny < m) and (rooms[x][ny] == '-'):
+        dfs(x, ny)
+  if (rooms[x][y] == '|'):
+    rooms[x][y] = 1 # 해당 노드 방문처리
+    for i in dx:
+      nx = x + i
+      if (nx > 0 and nx < n) and (rooms[nx][y] == '|'):
+        dfs(nx, y)
+
+for i in range(n):
+  for j in range(m):
+    if (rooms[i][j] == '-') or (rooms[i][j] == '|'):
+      dfs(i, j)
+      result += 1
+
+# 구현
 for i in range(n):
   a = ''
   for j in range(m):
