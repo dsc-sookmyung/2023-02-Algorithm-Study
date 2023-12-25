af0f8b53140a97262106098088f90c3350583c5e
Choi Yena <71956482+YenaChoi00@users.noreply.github.com>
2023-12-25 23:59:36 +0900

    Update A-29160.py

--- 06-BST-Priority_Queue-Graph-Tree/choiyn33/A-29160.py
+++ 06-BST-Priority_Queue-Graph-Tree/choiyn33/A-29160.py
@@ -1,16 +1,27 @@
 import sys
 
 n, k = map(int, input().split())
-arr = [[]] * 1000001
+arr = [] * 1000001
 
 for i in range(n):
     position, value = map(int, sys.stdin.readline().split())
-    arr[position] = value
-    arr.sort()
-print(arr)
+    arr.append = ((position, value))
 
-# year = 1
-# team = [[]] * 12    # 포지션 번호로 [0] 은 제외
-# while year <= k:
-#     for i in range(1, 12):
-#         team[i] = max(arr[i])
+year = 1
+while year <= k:
+    team = [0] * 12    # 포지션 번호로 [0] 은 제외
+
+    for i in range(1, 12):  # 팀 구성
+        team[i] = max_value = max(
+            (value for p, value in arr if p == i), default=0)
+
+    for i in range(11):  # 감소
+        team[i] = max(team[i] - 1, 0)
+
+    for i in range(1, 12):  # 재구성
+        team[i] = max_value = max(
+            (value for p, value in arr if p == i), default=0)
+
+    year += 1
+
+print(sum(team))
